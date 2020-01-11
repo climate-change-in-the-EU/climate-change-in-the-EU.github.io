e41ca59ba9049342b22a804598e31ba2e26cc0ac
ereuhl <ereuhl1@uni-koeln.de>
2020-01-11 18:02:05 +0100

    Fixed a path, reformatted

--- process_special_eb.py
+++ process_special_eb.py
@@ -13,21 +13,24 @@ sheets_interesting = {'most_serious_problem': ['QA1a', 'QB1a', 'QC1a', 'QE1a1', 
                       'personally_taken_action': ['QA5', 'QB5', 'QC5']}
 
 # Directory for input/original data and output/processed data
-dir_original_eb = '../climatechange/original_data/special_eb/'
-dir_processed_eb = '../climatechange/processed_data/special_eb/'
+dir_original_eb = 'original_data/special_eb/'
+dir_processed_eb = 'processed_data/special_eb/'
 
 
 def extract_to_csv(special_eb_file, sheet, question_short):
     question = []
     answers = []
+
     # Get the cell containing the question in English
     if sheet.cell(2, 11).value != "":
         question.append(sheet.cell(2, 11).value)
     else:
         question.append(sheet.cell(2, 10).value)
+
     # Get the rows containing the answers (Answers always fill the sheet from row 8 until the last row)
     for i in range(8, sheet.nrows):
         answers.append(sheet.row_values(i))
+
     # Write to CSV file
     csv_filename = dir_processed_eb + special_eb_file + '_' + question_short + '.csv'
     with open(csv_filename, 'wt') as f:
@@ -39,16 +42,15 @@ def extract_to_csv(special_eb_file, sheet, question_short):
 def process_special_eb():
     # Go through all files in the directory
     for special_eb_file in os.listdir(dir_original_eb):
+
         # Open the file as a workbook
         workbook = xlrd.open_workbook(dir_original_eb + special_eb_file)
-        '''# If your file has non-ASCII characters, you should open it in the unicode format as follows:
-        workbook = xlrd.open_workbook('my_file_name.xls', encoding='cp1252')
-        # If your spreadsheet is very large, you can add an argument on_demand, which loads only current sheets to memory:
-        workbook = xlrd.open_workbook('my_file_name.xls', on_demand = True)'''
+
         # Go through each sheet of the workbook
         for sheet_name in workbook.sheet_names():
             sheet = workbook.sheet_by_name(sheet_name)
-            # Filter out a sheet if it contains one of the specified questions
+
+            # Filter out a sheet and extract its data to CSV if it contains one of the specified questions
             for question_short, sheet_interesting in sheets_interesting.items():
                 if sheet_name in sheet_interesting:
                     extract_to_csv(special_eb_file, sheet, question_short)
